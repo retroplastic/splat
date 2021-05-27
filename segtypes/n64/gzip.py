5ba88fc0dc257d6111d8975d4af4feabaaaea0c9
retroplastic <retroplastic@protonmail.com>
2021-05-27 17:33:41 +0200

    segtypes: Implement gzip decompression.

--- segtypes/n64/gzip.py
+++ segtypes/n64/gzip.py
@@ -3,6 +3,8 @@ from typing import Optional
 from segtypes.n64.segment import N64Segment
 from util import options
 from util import log
+import subprocess
+import gzip
 
 class N64SegGzip(N64Segment):
     def out_path(self) -> Optional[Path]:
@@ -15,6 +17,17 @@ class N64SegGzip(N64Segment):
         if self.rom_end == "auto":
             log.error(f"segment {self.name} needs to know where it ends; add a position marker [0xDEADBEEF] after it")
 
+        compressed_bytes = rom_bytes[self.rom_start : self.rom_end]
         with open(path, "wb") as f:
-            f.write(rom_bytes[self.rom_start : self.rom_end])
+            f.write(compressed_bytes)
         self.log(f"Wrote {self.name} to {path}")
+
+        subprocess.call(["gunzip", "-k", "-N", path])
+
+        # TODO: Grab file name from gzip and write proper timestamp
+        """
+        decompress_path = options.get_asset_path() / self.dir / f"{self.name}.python.raw"
+        decompressed = gzip.decompress(compressed_bytes)
+        with open(decompress_path, 'wb') as f:
+          f.write(decompressed)
+        """
